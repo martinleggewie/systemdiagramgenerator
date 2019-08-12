d20cd780ce572f213f92c9007bdf3aeda8dec111
Martin Leggewie <trommelmeister@gmx.de>
2019-08-12 23:31:10 +0200

    improved/beautified the PlantUML output a little bit

--- src/main/java/org/codemaker/systemdiagramgenerator/PumlGenerator.java
+++ src/main/java/org/codemaker/systemdiagramgenerator/PumlGenerator.java
@@ -2,11 +2,9 @@ package org.codemaker.systemdiagramgenerator;
 
 import java.io.BufferedWriter;
 import java.io.File;
-import java.io.FileWriter;
 import java.io.IOException;
 import java.io.StringWriter;
 import java.util.Comparator;
-import java.util.stream.Collectors;
 
 public class PumlGenerator {
 
@@ -24,14 +22,18 @@ public class PumlGenerator {
         BufferedWriter writer = new BufferedWriter(stringWriter);
         writer.write(String.format("@startuml %s\n", new File(filename).getName()));
         writer.newLine();
-        for (Sys sys : dependyMatrix.getSyses()) {
-            writer.write(String.format("rectangle %s\n", sys.getName().replace("-", "")));
-        }
+
         int maxSysNameLength = dependyMatrix.getSyses().stream().max(Comparator.comparing(sys -> sys.getName().length())).get().getName().length();
+
+        for (Sys sys : dependyMatrix.getSyses()) {
+            String sysName = sys.getName();
+            String pumlCompatibleSysName = sysName.replace("-", "");
+            writer.write(String.format("rectangle %-" + maxSysNameLength + "s as %s\n", pumlCompatibleSysName, "\"" + sysName + "\""));
+        }
 
         writer.newLine();
         for (Dependy dependy : dependyMatrix.getDependies()) {
-            writer.write(String.format("%" +maxSysNameLength + "s --> %s\n", dependy.getFrom().getName().replace("-", ""), dependy.getTo().getName().replace("-", "")));
+            writer.write(String.format("%" + maxSysNameLength + "s --> %s\n", dependy.getFrom().getName().replace("-", ""), dependy.getTo().getName().replace("-", "")));
         }
         writer.newLine();
         writer.write("@enduml");
